0a30f23dd1dfea8145afc6096ac93589ae1c08e8
Askar Musthaffa <askarmus@hotmail.com>
2022-06-29 22:40:15 +0530

    dddd

--- partials/header.js
+++ partials/header.js
@@ -6,7 +6,7 @@ export const Header = () => (
       <Link href="/"> 
         <a   className="navbar-brand pe-3">
           <img
-            src="assets/image/how-we-work-section-bg-3.svg"
+            src="assets/image/logo.svg"
             width={200}
             alt="NavDeck"
           />
